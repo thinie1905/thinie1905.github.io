b892ca02de2fd0d62638745dff5c01935331a9f2
thinie1905 <35513491+thinie1905@users.noreply.github.com>
2018-12-02 04:18:31 +0800

    Update main.js

--- dap/main.js
+++ dap/main.js
@@ -32,11 +32,11 @@ function setTable(result, probs) {
     //loop over result of prediction
     for (var i = 0; i < result.length; i++) {
         if (k == 0 && result[i] == "hand"){
-            document.getElementById('prob1').innerHTML = (probs[i] * 100)
+            document.getElementById('prob1').innerHTML = (probs[i] * 100).toFixed(2)
         } else if (k == 1 && result[i] == "face"){
-             document.getElementById('prob2').innerHTML = (probs[i] * 100)
+             document.getElementById('prob2').innerHTML = (probs[i] * 100).toFixed(2)
         } else if (k == 2 && result[i] == "foot"){
-             document.getElementById('prob3').innerHTML = (probs[i] * 100)
+             document.getElementById('prob3').innerHTML = (probs[i] * 100).toFixed(2)
         }
     }
 
